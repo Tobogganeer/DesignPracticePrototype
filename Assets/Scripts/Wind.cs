b82f6e81a0c384544ac11ff7255d6226d8fa302f
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-10-04 15:30:44 -0400

    Set up wreck

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -10,5 +10,25 @@ public class Wind : MonoBehaviour
         instance = this;
     }
 
-    public static Vector2 CurrentWind => Vector2.zero;
+    public Vector2 changeTime = new Vector2(1f, 5f);
+    public float maxStrengthX = 2f;
+    public float maxStrengthY = 0.5f;
+
+    public static Vector2 CurrentWind;
+
+    Vector2 targetWind;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            targetWind = new Vector2(Random.Range(-maxStrengthX, maxStrengthX), Random.Range(-maxStrengthY, maxStrengthY));
+            yield return new WaitForSeconds(Random.Range(changeTime.x, changeTime.y));
+        }
+    }
+
+    private void Update()
+    {
+        CurrentWind = Vector2.Lerp(CurrentWind, targetWind, Time.deltaTime * 2f);
+    }
 }
