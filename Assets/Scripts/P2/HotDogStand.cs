45f57204a8ed4b1c2300b90b60ae83b57fb4cc0f
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-10-12 19:51:02 -0400

    Code for getting closest hot dog stand

--- Assets/Scripts/P2/HotDogStand.cs
+++ Assets/Scripts/P2/HotDogStand.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class HotDogStand : MonoBehaviour
 {
-    
+    public static List<HotDogStand> all = new List<HotDogStand>();
+
+    private void OnEnable()
+    {
+        all.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        all.Remove(this);
+    }
+
+    public static HotDogStand GetClosest(Vector3 position)
+    {
+        float closestDist = Mathf.Infinity;
+        HotDogStand closestStand = null;
+
+        foreach (HotDogStand stand in all)
+        {
+            float dist = Vector3.Distance(stand.transform.position, position);
+            if (dist < closestDist)
+            {
+                closestDist = dist;
+                closestStand = stand;
+            }
+        }
+
+        return closestStand;
+    }
 }
