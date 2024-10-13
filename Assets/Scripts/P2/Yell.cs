e99b7642f89971ad0bd2cab5af643a0d77ccd923
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-10-13 09:16:41 -0400

    Yelling

---
+++ Assets/Scripts/P2/Yell.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yell : MonoBehaviour
+{
+    public float speed = 5f;
+    public float growSpeed = 1f;
+
+    [HideInInspector]
+    public Vector3 direction;
+
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        transform.localScale += Vector3.one * growSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Person"))
+            collision.GetComponent<Person>().HitByYell(direction);
+    }
+}
--- Assets/Scripts/P2/Yelling.cs
+++ Assets/Scripts/P2/Yelling.cs
@@ -1,6 +1,8 @@
+using Popcron;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Gizmos = Popcron.Gizmos;
 
 public class Yelling : MonoBehaviour
 {
@@ -10,13 +12,48 @@ public class Yelling : MonoBehaviour
     public float cooldown = 1.5f;
     float timer;
 
+    Vector2 aimDirection = Vector2.right;
+
     private void Update()
     {
         timer -= Time.deltaTime;
 
+        UpdateAimDirection();
+
         if (Input.GetKeyDown(KeyCode.Space) && timer < 0f)
         {
-            
-        }    
+            timer = cooldown;
+            Quaternion rot = Quaternion.LookRotation(Vector3.forward, aimDirection);
+            GameObject yell = Instantiate(yellPrefab, transform.position, rot);
+            yell.GetComponent<Yell>().direction = aimDirection;
+        }
+
+        fillBar.fillAmount = timer / cooldown;
+        Gizmos.Line(transform.position, transform.position + (Vector3)aimDirection, Color.magenta);
+    }
+
+    private void OnRenderObject()
+    {
+        //Gizmos.Line(transform.position, transform.position + (Vector3)aimDirection, Color.magenta);
+    }
+
+    void UpdateAimDirection()
+    {
+        Vector2 desired = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            desired += Vector2.up;
+        if (Input.GetKey(KeyCode.DownArrow))
+            desired += Vector2.down;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            desired += Vector2.left;
+        if (Input.GetKey(KeyCode.RightArrow))
+            desired += Vector2.right;
+
+        desired.Normalize();
+
+        // Only update if we are aiming somewhere
+        if (desired.sqrMagnitude != 0)
+            aimDirection = Vector2.Lerp(aimDirection, desired, Time.deltaTime * 10f);
     }
 }
