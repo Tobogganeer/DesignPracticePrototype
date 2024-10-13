4504d92ab2e743d364747a80bacca45fa633f7c9
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-10-13 09:21:30 -0400

    Player movement

--- Assets/Scripts/P2/PlayerMovement.cs
+++ Assets/Scripts/P2/PlayerMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 2f;
+    public float acceleration = 6f;
 
-    // Update is called once per frame
-    void Update()
+    Vector2 velocity;
+
+    private void Update()
     {
-        
+        Vector2 desired = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        desired.Normalize();
+
+        velocity = Vector2.Lerp(velocity, desired * speed, Time.deltaTime * acceleration);
+        transform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
